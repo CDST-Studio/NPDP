0fd4d673f492fdd14666b87f41295ae2f5172e6a
ykm989 <ykm989@naver.com>
2021-10-08 01:03:04 +0900

    'forbae'

--- Web/charge/models.py
+++ Web/charge/models.py
@@ -1,12 +1,9 @@
 from django.db import models
 
 class Post(models.Model):
-    user_name = models.CharField(max_length = 30,blank = True)
     content = models.TextField(blank = True)
 
     image = models.ImageField(upload_to = 'Orders/',blank = True)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
 
-    def __str__(self):
-        return f'[{self.pk}] {self.user_name}'
--- Web/charge/views.py
+++ Web/charge/views.py
@@ -22,13 +22,13 @@ def complete(request):
 def upload_File(request):
     form = Post()
 
-    form.user_name = request.POST['user_name']
-    form.content = request.POST['content']
+    #form.content = request.POST['content']
 
-    try:
+    form.image = request.FILES['image']
+    '''try:
         form.image = request.FILES['image']
     except:
-        pass
+        pass'''
 
     form.save()
 
